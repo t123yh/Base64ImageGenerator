da418783fdcff7af404a8b2d3cca8e7da20965a7
t123yh <t123yh@outlook.com>
2016-02-12 20:54:03 +0800

    完成第一版

---
+++ Base64ImageGenerator/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Base64ImageGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: <InputFileName> <Ratio> <OutputHtmlPath>\nRatio is a float number. e.g. 0.5\nThe base64 string will be put into standard output stream.");
+                return;
+            }
+
+            double ratio = double.Parse(args[1]);
+            Bitmap newImage;
+            using (var image = Image.FromFile(args[0]))
+            {
+                // Consolas font ratio: 1:2
+                newImage = ResizeImage(image, (int)(image.Width * ratio), (int)(image.Height * ratio / 2));
+            }
+            //newImage.Save("D:\\test.png", ImageFormat.Png);
+            byte[] resultBytes;
+            using (FileStream fs = new FileStream(args[2], FileMode.OpenOrCreate, FileAccess.Write))
+            using (MemoryStream ms = new MemoryStream(newImage.Height * newImage.Width * 3))
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.WriteLine("<html>");
+                writer.WriteLine(Properties.Resources.Header);
+                writer.WriteLine("<body>");
+                writer.WriteLine("<div class=\"asciiimg\">");
+                ms.Write(BitConverter.GetBytes(newImage.Width), 0, 4);
+                ms.Write(BitConverter.GetBytes(newImage.Height), 0, 4);
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    for (int x = 0; x < newImage.Width; x++)
+                    {
+                        Color color = newImage.GetPixel(x, y);
+                        ms.WriteByte(color.R);
+                        ms.WriteByte(color.G);
+                        ms.WriteByte(color.B);
+                        //ms.WriteByte((byte)(x % 255));
+                        //ms.WriteByte((byte)(y % 255));
+                        //ms.WriteByte(0);
+                        writer.Write("<span style=\"color: #{0:X2}{1:X2}{2:X2}\">#</span>", color.R, color.G, color.B);
+                    }
+                    writer.WriteLine("<br>");
+                }
+                writer.WriteLine("</div></html>");
+                resultBytes = new byte[ms.Length];
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.Read(resultBytes, 0, resultBytes.Length);
+            }
+            Console.WriteLine(Convert.ToBase64String(resultBytes));
+
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
---
+++ Base64ImageGenerator/WebScripts/Unwrapper.js
@@ -0,0 +1,49 @@
+﻿function btoab(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+
+}
+function thx(bytes) {
+    return Array.from(bytes, function (byte) {
+        return ('00' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('').toUpperCase();
+}
+
+$(function () {
+    var url = "your_url";
+    $.get(url, function (data) {
+        var bdoc = $('<div id="body-mock">' + data.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/ig, '') + '</div>');
+        var imageBase64 = $.trim(bdoc.find("#your_div_containing_base64_text_generated_by_this_app_representing_image").text());
+        var buffer = btoab(imageBase64);
+        var intReader = new Int32Array(buffer);
+        var imgWidth = intReader[0];
+        var imgHeight = intReader[1];
+        var bitmapArray = new Uint8Array(buffer, 8, buffer.byteLength - 8);
+        $(".left-topcon").click(function () {
+            $("#leftpic").hide();
+            $("#rightpic").hide();
+            var imgDiv = $("#pcList");
+            imgDiv.removeAttr("style");
+            imgDiv.removeAttr("videoList-container");
+            imgDiv.attr("id", "hahaha");
+            imgDiv.empty();
+            imgDiv.css('font-family', 'Consolas, monospace, sans-serif');
+            imgDiv.css('font-size', 'xx-small');
+            for (var y = 0, num = 0; y < imgHeight; y++) {
+                var columnDiv = $('<div/>');
+                for (var x = 0; x < imgWidth; x++, num += 3) {
+                    var color = '#' + thx(bitmapArray.subarray(num, num + 3));
+                    $('<span/>').css("color", color).text("#").appendTo(columnDiv);
+                }
+                columnDiv.appendTo(imgDiv);
+            }
+        });
+    });
+});
+
+
